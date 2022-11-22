9bef299bfebe79591800955b87bcf7fb5b646c14
Gustavo Ferreira <53050800+Gustavowp8@users.noreply.github.com>
2022-11-22 03:01:53 -0800

    Add files via upload

--- funcao/app.js
+++ funcao/app.js
@@ -1,3 +1,15 @@
 function aviso(){
     alert('Indiponivel')
 }
+
+function contaN(){
+    alert('Para criar uma nova conta falei com o administrador')
+}
+
+function fecha(){
+    avisoUm.style.display = 'none'
+}
+
+function recur(){
+    avisoUm.style.display = 'block'
+}
