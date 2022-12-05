5e9bf20ba357e4dee21a4590d6501d42466b42cf
Gustavo Ferreira <gustavoferreira73@live.com>
2022-12-05 20:58:54 -0300

    atualização

--- funcao/app.js
+++ funcao/app.js
@@ -1,5 +1,38 @@
 function aviso(){
-    alert('Indiponivel')
+    //alert('Indiponivel')
+
+    var email = document.getElementById('email').value;
+    var passas = document.getElementById('pass').value;
+
+    if(email == "" || passas == ""){
+        alert('Negado')
+    }
+    else if(email != "adm@adm.com" || passas != "123456"){
+        alert('E-mail errado ou senha')
+    }
+    else if(email == "adm@adm.com" || passas == "123456"){
+        alert('O banco de dados esta desativado!')
+        location="painel.html"
+    }
+}
+
+function cSair(){
+    var sair = confirm('Tem certeza que deseja sair ?')
+    if(sair == true){
+        location='index.html'
+    }
+}
+
+function sAvd(){
+    alert('Sem acesso ao banco, fale com o programador!')
+}
+
+function config(){
+    alert('Configuração disponivel somente na proxima atualização!')
+}
+
+function chamaMenu(){
+    menu.style.display="block"
 }
 
 function contaN(){
