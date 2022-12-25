938c841a1af1f50be2b22aee7f310649fb372604
Gustavo Ferreira <gustavoferreira73@live.com>
2022-12-25 13:38:24 -0300

    Notas de avisos

--- funcao/app.js
+++ funcao/app.js
@@ -3,3 +3,11 @@ avisoaouser.addEventListener('click', fecharaviso);
 function fecharaviso(){
     sInfo.style.display='none'
 }
+
+function chamainfo(){
+    sInfo.style.display='block'
+}
+
+function infoS(){
+    alert("Indisponivel")
+}
